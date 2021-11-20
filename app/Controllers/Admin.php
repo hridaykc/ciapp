981e01114dbd29b74a6a208d4612bc1f5afc92e5
Hari KC <hridaykc@gmail.com>
2021-11-20 16:16:22 +0400

    update the Admin controller

---
+++ app/Controllers/Admin.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Controllers;
+use App\Controllers\BaseController;
+
+Class Admin extends BaseController{
+
+    public function __construct(){
+        // loads the defaults files.
+    }
+    public function index(){
+        // Here will loads dashboard if not logged in
+        
+    }
+    public function login(){
+        // login page then check the login
+    }
+
+    public function contents(){
+        // shows the website contents
+    }
+
+    public function blogs(){
+        // updates the contents blogs
+    }
+}
+
+?>
